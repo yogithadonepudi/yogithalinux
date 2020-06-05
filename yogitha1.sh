ec151eaac19feba68b4a90a3664783843843bc99
yogithadonepudi <66406759+yogithadonepudi@users.noreply.github.com>
2020-06-06 02:08:16 +0530

    Add files via upload

---
+++ yogitha1.sh
@@ -0,0 +1,24 @@
+[3J[H[2J[32mInternet: (B[m Connected
+[32mOperating System Type : (B[m GNU/Linux
+[32mOS Name : (B[mLinux Mint
+UBUNTU_CODENAME=bionic
+[32mOS Version : (B[m19.2 (Tina)
+[32mArchitecture : (B[m x86_64
+[32mKernel Release : (B[m 4.15.0-54-generic
+[32mHostname : (B[m linux-VirtualBox
+[32mInternal IP : (B[m 192.168.2.11
+[32mExternal IP : (B[m 183.82.158.53
+[32mName Servers : (B[m This internal configured Run currently Third symlink replace See operation 127.0.0.53 edns0
+[32mLogged In users : (B[m
+linux    tty7         2020-06-05 22:51 (:0)
+[32mRam Usages : (B[m
+              total        used        free      shared  buff/cache   available
+Mem:           985M        711M         60M         37M        213M        101M
+[32mSwap Usages : (B[m
+              total        used        free      shared  buff/cache   available
+Swap:          2.0G        617M        1.4G
+[32mDisk Usages : (B[m
+Filesystem      Size  Used Avail Use% Mounted on
+/dev/sda1        64G  7.8G   53G  13% /
+[32mLoad Average : (B[m average:0.13,0.85,
+[32mSystem Uptime Days/(HH:MM) : (B[m 54 min
